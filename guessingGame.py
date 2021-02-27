22a8da2f282d3e627be875e3bd65dc1da220746f
MelvynnHadia <69381490+MelvynnHadia@users.noreply.github.com>
2021-02-27 12:13:15 +0530

    Add files via upload

---
+++ guessingGame.py
@@ -0,0 +1,24 @@
+win = 9
+guesstheno = int(input("Enter a Number: "))
+
+if(guesstheno==win):
+    print("You Won")
+elif(guesstheno!==win):
+    print("That Wasn't the Correct Number")
+guesstheno = int(input("Enter a Number: "))
+elif(guesstheno!==win): 
+    print("That Wasn't the Correct Number")
+guesstheno = int(input("Enter a Number: "))
+elif(guesstheno!==win): 
+    print("That Wasn't the Correct Number")
+guesstheno = int(input("Enter a Number: "))
+elif(guesstheno!==win): 
+    print("That Wasn't the Correct Number")
+guesstheno = int(input("Enter a Number: "))
+elif(guesstheno!==win): 
+    print("That Wasn't the Correct Number")
+    guesstheno = int(input("Enter a Number: "))
+else:
+    print(You Lose)
+
+        
